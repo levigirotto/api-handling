c50d1e171c5046faeab3920fb34965a0f3cc470b
Levi Girotto <girottolevi@gmail.com>
2025-08-21 15:17:58 -0300

    .

--- api/gamesCardProxy.ts
+++ api/gamesCardProxy.ts
@@ -5,7 +5,8 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
     const response = await axios.get("https://www.freetogame.com/api/games");
     res.status(200).json(response.data);
-  } catch (error) {
+  } catch (error: any) {
+    console.error("Error fetching FreeToGame API:", error.message);
     res.status(500).json({ error: error.message });
   }
 }
--- src/components/cards/ISSCard.tsx
+++ src/components/cards/ISSCard.tsx
@@ -5,7 +5,6 @@ import axios from "axios";
 import "leaflet/dist/leaflet.css";
 import ApiCard from "@/components/cards/ApiCard.tsx"
 import MapUpdater from "@/components/MapUpdater";
-import { Button } from "../ui/button";
 
 const issIcon = L.icon({
   iconUrl: "/iss.png",
@@ -16,48 +15,59 @@ const issIcon = L.icon({
 
 export default function ISSCard() {
   const [position, setPosition] = useState<[number, number]>([0, 0]);
+  const [altitude, setAltitude] = useState<number>(0);
+  const [velocity, setVelocity] = useState<number>(0);
 
-  async function fetch() {
+  useEffect(() => {
+    async function fetch() {
       try {
         const res = await axios.get(
-          "http://api.open-notify.org/iss-now.json"
+          "https://api.wheretheiss.at/v1/satellites/25544"
         );
-        const data = res.data.iss_position;
+        const data = res.data;
         setPosition([data.latitude, data.longitude]);
+        setAltitude(data.altitude);
+        setVelocity(data.velocity);
       } catch (error) {
         console.error("Failed to fetch ISS location", error);
       }
     }
 
-  useEffect(() => {
     fetch();
+    const interval = setInterval(fetch, 5000);
+    return () => clearInterval(interval);
   }, []);
 
   return (
     <ApiCard 
-      title="ISS Current Location"
-      description="ISS Current Location"
-      apiLink="http://open-notify.org/Open-Notify-API/ISS-Location-Now/"
+      title="ISS Whereabouts"
+      description="Where the ISS at?"
+      apiLink="https://wheretheiss.at/w/developer"
       content={
-        <MapContainer
-          center={position}
-          zoom={4}
-          scrollWheelZoom={true}
-          className="rounded-xl overflow-hidden z-0"
-          style={{ height: 300, width: "100%" }}
-        >
-          <TileLayer
-            attribution='&copy; OpenStreetMap contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={position} icon={issIcon}>
-            <Popup>International Space Station</Popup>
-          </Marker>
-          <MapUpdater position={position} />
-        </MapContainer>
-        
+        <div className="flex flex-col gap-3">
+          <MapContainer
+            center={position}
+            zoom={2}
+            scrollWheelZoom={true}
+            className="rounded-xl overflow-hidden z-0"
+            style={{ height: 300, width: "100%" }}
+          >
+            <TileLayer
+              attribution='&copy; OpenStreetMap contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position} icon={issIcon}>
+              <Popup>International Space Station</Popup>
+            </Marker>
+            <MapUpdater position={position} />
+          </MapContainer>
+          <div className="container flex flex-col gap-2 bg-zinc-800 p-4 rounded-xl text-zinc-400">
+            <p><strong>Coordinates:</strong><br />Lat: {position[0]},<br />Lon: {position[1]}</p>
+            <p><strong>Altitude:</strong> {altitude.toFixed(2)}km</p>
+            <p><strong>Velocity:</strong> {velocity.toFixed(2)}km/h</p>
+          </div>
+        </div>
       }
-      footer={<Button onClick={fetch}>Fetch</Button>}
       />
   );
 }
