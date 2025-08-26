f3305c9aaa451e8e4abf88f106c4120201367cab
Levi Girotto <girottolevi@gmail.com>
2025-08-26 09:41:52 -0300

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export function Header() {
     <header className="fixed top-0 left-0 right-0 z-50 flex items-center bg-zinc-950 justify-center w-full py-4">
       <div className="container mx-auto px-4 flex justify-center">
         <img src={Logo} alt="Website Logo" className="h-8 w-auto absolute left-4" />
-        <h1 className="py-0.5 text-xl font-semibold text-zinc-50">API Cards</h1>
+        <h2 className="py-0.5 text-xl font-semibold text-zinc-50">API Cards</h2>
       </div>
     </header>
   )
