cafecc62ac1ff0733425f8bff1521d1fbe67c263
Levi Girotto <girottolevi@gmail.com>
2025-09-25 11:13:23 -0300

    .

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "next-themes";
 import App from "./App";
 import "@/styles/index.css";
-import "leaflet/dist/leaflet.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
